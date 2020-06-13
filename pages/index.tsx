189e67b272cf2769f3fbeff18ab17a21263fe013
Pedro Henrique Jansen Leite <pedrohjleite@Pedros-MacBook-Pro.local>
2020-06-13 01:40:16 -0300

    co-author github.com/viniaxt

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import GlobalStyle from '../styles/global';
+import { useState } from 'react'
+
+import { 
+  Container,
+  ColorPickerInput,
+  Color,
+  ColorsContainer,
+  ColorText,
+  Title,
+  Names,
+  Nois
+} from './styles'
+
+const App: React.FC = () => {
+  const [colors, setColors] = useState([])
+
+  return (
+    <>
+      <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet"></link>
+      <GlobalStyle />
+      <Container>
+        <Title>Color Picker</Title>
+        <ColorPickerInput 
+          type="color"
+          onChange={(event) =>
+            setColors([
+              ...colors, 
+              { backgroundColor: event.target.value}
+            ])
+          }
+        />
+        <ColorsContainer>
+          {colors.map(({ backgroundColor }) => (
+            <Color key={backgroundColor} nome={backgroundColor}>
+              <ColorText>
+                {backgroundColor.toUpperCase()}
+              </ColorText>
+            </Color>
+          ))}
+        </ColorsContainer>
+        <Names>
+          <Nois href="https://github.com/pedrohjleite">Pedro Leite</Nois> <Nois href="https://github.com/viniaxt">Vinícius Axt</Nois>
+        </Names>
+      </Container>
+   </>
+  )
+}
+
+export default App;
---
+++ pages/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+interface ColorProps {
+  nome: string;
+}
+
+export const Title = styled.h1`
+  color: #fff;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
+`;
+
+export const ColorPickerInput = styled.input`
+  width: 100px;
+  height: 100px;
+  border: 2px solid black;
+  margin-bottom: 24px;
+  margin-top: 24px;
+`
+
+export const ColorText = styled.h3`
+  background-color: white;
+  border-radius: 10px 0 0 0;
+  padding: 10px;
+`
+
+export const Color = styled.div<ColorProps>`
+  background-color: ${({ nome }) => nome};
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+
+
+  width: 18vw;
+  height: 20vh;
+`
+
+export const ColorsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 72vw;
+`
+
+export const Names = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 30vw;
+`
+
+export const Nois = styled.a`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  font-size: 20 px;
+  color: #fff;
+`;
